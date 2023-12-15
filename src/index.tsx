5bddf9cf3b2d12ae60fd6cb9e903f066d6a148ba
HJVeronica <hj.veronica.shim@gmail.com>
2023-12-15 18:19:03 +0900

    사용할 라우팅 경로 셋업

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <div>Main Home</div>,
+  },
+  {
+    path: "/posts",
+    element: <div>Post List</div>,
+  },
+  {
+    path: "/posts/:id",
+    element: <div>Post Contents</div>,
+  },
+  {
+    path: "/posts/new",
+    element: <div>Post New</div>,
+  },
+  {
+    path: "/posts/edit/:id",
+    element: <div>Post Edit</div>,
+  },
+  {
+    path: "/profile",
+    element: <div>Profile</div>,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" />,
   },
 ]);
 
