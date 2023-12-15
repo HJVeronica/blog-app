029a38ea5aca7c573d76aeb8e4d19223b53ac644
HJVeronica <hj.veronica.shim@gmail.com>
2023-12-15 18:09:14 +0900

    react-router-dom 설치 및 기본 root 라우팅 셋업

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Main Home</div>,
+  },
+  {
+    path: "/posts",
+    element: <div>Post List</div>,
+  },
+  {
+    path: "/posts/:id",
+    element: <div>Post Contents</div>,
+  },
+  {
+    path: "/posts/new",
+    element: <div>Post New</div>,
+  },
+  {
+    path: "/posts/edit/:id",
+    element: <div>Post Edit</div>,
+  },
+  {
+    path: "/profile",
+    element: <div>Profile</div>,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
