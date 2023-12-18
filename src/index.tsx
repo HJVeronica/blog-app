c4f38187752c2d69c19f1c0fb5bd9aa4825bde1b
HJVeronica <hj.veronica.shim@gmail.com>
2023-12-18 16:40:08 +0900

    PostDetail 페이지 파일 구조 변경

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import "index.css";
 
 import Home from "pages/home";
 import PostList from "pages/posts";
-import PostPage from "components/PostPage";
+import PostPage from "pages/posts/detail";
 import PostNew from "pages/posts/new";
 import PostEdit from "pages/posts/edit";
 import ProfilePage from "components/ProfilePage";
