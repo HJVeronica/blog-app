1e8897790027ffc67d2e8a81751fb9587f5e879c
HJVeronica <hj.veronica.shim@gmail.com>
2023-12-18 17:26:06 +0900

    회원가입 페이지 디자인 구현

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Header from "components/Header";
+import SignupForm from "components/SignupForm";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <Header />
+      <SignupForm />
+    </>
+  );
 };
 
 export default SignUp;
