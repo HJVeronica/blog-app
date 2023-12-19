8e11736d54185cd01e1aa7067c4d3666b863adb3
HJVeronica <hj.veronica.shim@gmail.com>
2023-12-19 13:23:13 +0900

    auth 정보를 context API로 구현

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+
 import { app } from "firebaseApp";
 import { getAuth, signOut } from "firebase/auth";
 
 import { toast } from "react-toastify";
 
+import AuthContext from "context/AuthContext";
+
 const Profile = () => {
-  const auth = getAuth(app);
+  const { user } = useContext(AuthContext);
 
   const onSignOut = async () => {
     try {
@@ -21,10 +25,8 @@ const Profile = () => {
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
-          <div className="profile__email">{auth?.currentUser?.email}</div>
-          <div className="profile__name">
-            {auth?.currentUser?.displayName || "사용자"}
-          </div>
+          <div className="profile__email">{user?.email}</div>
+          <div className="profile__name">{user?.displayName || "사용자"}</div>
         </div>
       </div>
 
