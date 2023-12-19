e970520722e694aa6aff0e24d351f7c3b83b5123
HJVeronica <hj.veronica.shim@gmail.com>
2023-12-19 11:35:19 +0900

    로그인 동작 구현 (Firebase Auth)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Router from "Router";
 
 const App = () => {
   const auth = getAuth(app);
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [init, setInit] = useState<boolean>(false); // auth 초기화 전 확인 용도
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
 
-  return <Router isAuthenticated={isAuthenticated} />;
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+
+      setInit(true);
+    });
+  }, [auth]);
+
+  return (
+    <>
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : "Loading..."}
+    </>
+  );
 };
 
 export default App;
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,18 +1,96 @@
-import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+import { app } from "firebaseApp";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+import { toast } from "react-toastify";
 
 const LoginForm = () => {
+  const [error, setError] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const form = useRef<HTMLFormElement>(null);
+  const navigate = useNavigate();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    switch (name) {
+      case "email":
+        setEmail(value);
+        const validRegex =
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if (!value?.match(validRegex)) {
+          setError("이메일 형식이 올바르지 않습니다.");
+        } else {
+          setError("");
+        }
+
+        break;
+      default:
+        setPassword(value);
+        if (value?.length < 8) {
+          setError("비밀번호는 8자 이상이어야 합니다.");
+        } else {
+          setError("");
+        }
+
+        break;
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth(app);
+      await signInWithEmailAndPassword(auth, email, password);
+
+      toast.success("로그인 성공!");
+      form.current?.reset();
+    } catch (error: any) {
+      setError(error?.message);
+      toast.error(error?.code);
+    }
+  };
+
   return (
-    <form action="/post" method="POST" className="form form-lg">
+    <form onSubmit={onSubmit} className="form form-lg" ref={form}>
       <h1 className="form__title">로그인</h1>
       <div className="form__block">
         <label htmlFor="email">이메일</label>
-        <input type="email" name="email" id="email" required />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          onChange={onChange}
+          value={email}
+        />
       </div>
 
       <div className="form__block">
         <label htmlFor="password">비밀번호</label>
-        <input type="password" name="password" id="password" required />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          onChange={onChange}
+          value={password}
+        />
       </div>
+
+      {error && error?.length > 0 && (
+        <div className="form__block">
+          <div className="form__error">{error}</div>
+        </div>
+      )}
 
       <div className="form__block">
         계정이 없으신가요?{" "}
@@ -22,7 +100,12 @@ const LoginForm = () => {
       </div>
 
       <div className="form__block">
-        <input type="submit" value="로그인" className="form__btn--submit" />
+        <input
+          type="submit"
+          value="로그인"
+          className="form__btn--submit"
+          disabled={error?.length > 0}
+        />
       </div>
     </form>
   );
