a9f478dc1926feb35326fcfc88e0694b9a68db7f
HJVeronica <hj.veronica.shim@gmail.com>
2023-12-20 10:21:31 +0900

    테마 전환 구현 (다크모드) - ContextAPI 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import ThemeContext from "context/ThemeContext";
+
 import Router from "Router";
 import Loader from "components/Loader";
 
 const App = () => {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
   const [init, setInit] = useState<boolean>(false); // auth 초기화 전 확인 용도
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
@@ -28,10 +31,10 @@ const App = () => {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "light" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 };
 
